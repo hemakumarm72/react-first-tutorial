4cd40f8c71e070e4e51cf5569089e06416d0f1c6
unknown <hemakumarm72@gmail.com>
2021-03-27 12:25:17 +0530

    navbar.jsx add

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container" />
+      </nav>
+    </>
+  );
+}
+
+export default navbar;
