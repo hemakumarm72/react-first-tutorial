d897160c8fb27618e4dfc864884c206b7e8f2b3f
unknown <hemakumarm72@gmail.com>
2021-03-27 19:08:57 +0530

    content section add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/navbar/navbar';
 import Sidebar from './components/banner/banner';
+import Content from './components/content/content';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <div className="site">
       <Navbar />
       <Sidebar />
+      <Content />
     </div>
 
   );
---
+++ src/components/content/content.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './content.css';
+
+function content() {
+  return (
+    <div className="content">
+      <Contentsection />
+
+    </div>
+  );
+}
+
+const Contentsection = () => (
+  <p>content</p>
+);
+
+export default content;
