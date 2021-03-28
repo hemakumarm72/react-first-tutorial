c762f494df2df9ab08d7e16d994bd9050638dcd1
unknown <hemakumarm72@gmail.com>
2021-03-28 09:18:18 +0530

    content and card add

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -5,13 +5,42 @@ function content() {
   return (
     <div className="content">
       <Contentsection />
+      <Contentcard />
 
     </div>
   );
 }
 
 const Contentsection = () => (
-  <p>content</p>
+  <div className="content-section">
+    <p className="content-para">Our Service</p>
+    <br />
+    <p>
+      Top 100 Television Movies. Best of Rotten Tomatoes.
+      <br />
+      {' '}
+      Movies with 40 or more critic reviews vie for their place in history at Rotten Tomatoes.
+      {' '}
+      <br />
+      Eligible movies are ...
+    </p>
+  </div>
+
 );
 
+const Contentcard = () => (
+  <div className="card-container">
+    <Cardsection image="http://placeimg.com/280/400/spiderman" />
+    <Cardsection image="http://placeimg.com/280/400/avergas" />
+    <Cardsection image="http://placeimg.com/280/400/animals" />
+    <Cardsection image="http://placeimg.com/280/400/dogs" />
+    <Cardsection image="http://placeimg.com/280/400/moon" />
+
+  </div>
+
+);
+
+const Cardsection = ({ image }) => (
+  <div className="card" style={{ backgroundImage: `url(${image})` }} />
+);
 export default content;
